f991a3418426784e88384055e59eb2be7b22f94a
Trung Anht <frontendstack2002@gmail.com>
2022-08-15 20:35:15 +0700

    Design header day1

---
+++ src/components/Button/index.js
@@ -0,0 +1,16 @@
+// import classNames from 'classnames/bind'
+// import styles from "./button.module.scss"
+
+// const cx = classNames.bind(styles)
+
+// function Button({to, href,children, onClick}) {
+//     let Comp = 'h4'
+
+//     const classes = cx('wrapper')
+//     return (
+//         <Comp className={classes} >
+//             <span className={cx('learn')}>{children}</span>
+//         </Comp>
+//     )
+// }
+// export default Button
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,6 +1,11 @@
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChalkboard,faChalkboardUser,faBook } from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind'
 import styles from './Header.module.scss'
+
+import Tippy from '@tippyjs/react';
+import 'tippy.js/dist/tippy.css'; 
 
 const cx = classNames.bind(styles)
 
@@ -8,6 +13,27 @@ function Header() {
     return (
         <header className={cx('wrapper')}>
             <div className={cx('inner')}>
+                <div className={cx('logo')}>
+                    <h1 className={cx('logo-text')}>Voever</h1>
+                </div>
+                <div className={cx('service')}>
+                    <span className={cx('service-learn')}>
+                        <FontAwesomeIcon className={cx('icon-learn')} icon={faChalkboard}/>
+                        <h4>Học</h4>
+                    </span>
+                    <span className={cx('service-story')}>
+                        <FontAwesomeIcon className={cx('icon-story')} icon={faBook} />
+                        <h4>Truyện</h4>
+                    </span>
+                    <span className={cx('service-introduce')}>
+                        <FontAwesomeIcon className={cx('icon-introduce')} icon={faChalkboardUser} />
+                        <h4>Giới thiệu</h4>
+                    </span>
+                </div>
+                <div className={cx('action')}>
+                    <h4 className={cx('action-signUp')}>Đăng nhập</h4>
+                    <h4 className={cx('action-logIn')}>Đăng ký</h4>
+                </div>
                 {/* {logo} */}
                 {/* {Search} */}
             </div>
