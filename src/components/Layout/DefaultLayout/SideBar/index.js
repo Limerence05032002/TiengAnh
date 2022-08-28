b34a2ff6a3010185225f1601d6a8b8db807f9952
Trung Anht <frontendstack2002@gmail.com>
2022-08-28 20:27:25 +0700

    Sidebar tập 1

--- src/components/Layout/DefaultLayout/SideBar/index.js
+++ src/components/Layout/DefaultLayout/SideBar/index.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faRankingStar, faTrophy, faChartLine,faCaretRight, faMagnet, faArchway, faAtom} from '@fortawesome/free-solid-svg-icons';
 import classNames from 'classnames/bind'
 import styles from './SideBar.module.scss'
 
@@ -5,9 +7,50 @@ const cx = classNames.bind(styles)
 
 function SideBar() {
     return (
-        <aside className={cx('wrapper')}>
-            <h2>SideBar</h2>
-        </aside>
+        <div className={cx('wrapper')}>
+            <div className={cx('rank')}>
+                <div className={cx('wrapp_rank')}>
+                    <FontAwesomeIcon className={cx('rank_icon')} icon={faRankingStar}></FontAwesomeIcon>
+                    <h2 className={cx('rank_text')}>Xếp hạng để thách đấu</h2>
+                    <p className={cx('rank_decription')}>Hoàn thành bài học, leo rank tìm kiếm Thách đấu !</p>
+                </div>
+            </div>
+
+            <div className={cx('activity')}>
+                <div className={cx('wrapp_activity')}>
+                    <h2 className={cx('activity_text')}>Chuỗi hoạt động</h2>
+                    <div className={cx('activity_content')}>
+                        <FontAwesomeIcon className={cx('activity_icon')} icon={faChartLine}/>
+                        <p className={cx('activity_decription')}>Charts</p>
+                    </div>
+                    <span className={cx('more_activity')}>
+                        <p className={cx('more_text')}>Xem chi tiết </p>
+                        <FontAwesomeIcon className={cx('more_icon')} icon={faCaretRight}></FontAwesomeIcon>
+                    </span>
+                </div>
+            </div>
+
+            <div className={cx('achievement')}>
+                <div className={cx('wrapp_achievement')}>
+                    <h2 className={cx('achievement_text')}>Thành tích đạt được</h2>
+                    <FontAwesomeIcon className={cx('achievement_icon')} icon={faTrophy}/>
+                    <FontAwesomeIcon className={cx('achievement_icon')} icon={faMagnet}/>
+                    <FontAwesomeIcon className={cx('achievement_icon')} icon={faArchway}/>
+                    <FontAwesomeIcon className={cx('achievement_icon')} icon={faAtom}/>
+                    <p className={cx('achievement_decription')}>Chinh phục từ vựng chưa bao giờ dễ như vậy !</p>
+                    <span className={cx('more_activity')}>
+                        <p className={cx('more_text')}>Xem chi tiết </p>
+                        <FontAwesomeIcon className={cx('more_icon')} icon={faCaretRight}></FontAwesomeIcon>
+                    </span>
+                </div>
+            </div>
+
+            <div className={cx('charts')}>
+                <div className={cx('wrapp_charts')}>
+                    <h2 className={cx('charts_text')}>Xếp hạng</h2>
+                </div>
+            </div>
+        </div>
     )
 }
 export default SideBar
