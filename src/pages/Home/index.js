5b1228f21d02720040b4882dc6c491d9add5108b
NEWUSER <frontendstack2002@gmail.com>
2022-09-19 21:48:05 +0700

    Thiết kế UI Page Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,21 @@
 import Barchart from "~/components/Layout/DefaultLayout/SideBar/chart"
+import classNames from 'classnames/bind'
+import Styles from './home.scss'
+
+const cx = classNames.bind(Styles)
+
 
 function Home() {
     return (
         <div>
-            <h1>Home page</h1>
+            <nav className={cx("navmenu")}>
+                <ul>
+                    <li className={cx("active right1")}>Unit 1</li>
+                    <li className={cx("left")}>Unit 2</li>
+                    <li className={cx("right")}>Unit 3</li>
+                    
+                </ul>
+            </nav>
         </div>
     ) 
 }
